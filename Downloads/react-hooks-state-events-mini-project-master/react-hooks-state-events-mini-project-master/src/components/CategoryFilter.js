616b8f4a1b1b79ef6f1d5b02fdeb2f381019d339
sheema <sheemashamilfarook@gmail.com>
2023-01-22 17:01:36 +0300

    completed assignment

---
+++ Downloads/react-hooks-state-events-mini-project-master/react-hooks-state-events-mini-project-master/src/components/CategoryFilter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CategoryFilter() {
+  return (
+    <div className="categories">
+      <h5>Category filters</h5>
+      {/* render <button> elements for each category here */}
+    </div>
+  );
+}
+
+export default CategoryFilter;
---
+++ Downloads/react-hooks-state-events-mini-project-master/react-hooks-state-events-mini-project-master/src/components/TaskList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TaskList() {
+  return (
+    <div className="tasks">
+      {/* display a list of tasks using Task component */}
+    </div>
+  );
+}
+
+export default TaskList;
