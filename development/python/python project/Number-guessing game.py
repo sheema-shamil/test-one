588f61f7bc0280eeff7a627959b0e36ad2a97fe5
sheema <sheemashamilfarook@gmail.com>
2023-10-25 21:20:50 +0300

    completed

--- development/python/python project/Number-guessing game.py
+++ development/python/python project/Number-guessing game.py
@@ -1,2 +1,15 @@
 import random
 
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print('please type a number larger than 0 next time. ')
+        quit()
+else:
+    print('please type a number next time .')
+    quit()
+
+random_number = random
